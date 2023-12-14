a80cacdaebffaf9eb8e8fd95333df8212e5f3737
Michell Ayala <mich@rxrise.com>
2023-12-14 12:13:37 -0600

    fix specs

--- test/mocha/gilded-rose.spec.ts
+++ test/mocha/gilded-rose.spec.ts
@@ -1,10 +1,11 @@
-import { expect } from 'chai';
-import { Item, GildedRose } from '@/gilded-rose';
+import { expect } from "chai";
+import { GildedRose } from "@/gilded-rose";
+import { Item } from "@/entities/item";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+describe("Gilded Rose", () => {
+  it("should foo", () => {
+    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal('fixme');
+    expect(items[0].name).to.equal("fixme");
   });
 });
